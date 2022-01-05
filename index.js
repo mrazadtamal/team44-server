8ad0af1ad526234bf4bc63460b0136e33bd02654
mrazadtamal <mrazadt@gmail.com>
2022-01-05 19:15:59 +0600

    fix

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const postRoute = require("./routes/posts");
 const categoryRoute = require("./routes/categories");
 const multer = require("multer");
 const path = require("path");
+const cors = require("cors");
 
 dotenv.config();
 app.use(express.json());
@@ -37,6 +38,7 @@ app.post("/api/upload", upload.single("file"), (req, res) => {
   res.status(200).json("File has been uploaded");
 });
 
+app.use(cors());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
